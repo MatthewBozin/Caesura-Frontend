0a02a0d9011d0adf882e0bec95e682e605dcb5e9
Matthew Bozin <mbozin94@gmail.com>
2022-09-22 13:25:50 -0700

    Added Material UI card styling to Poem.js.

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import { Typography, Card, CardContent, Grid, TextField, Button } from '@mui/material'
 
 const Poem = (props) => {
   return (
-    <div>
-        {props.page === 'feed' && <h3>By {props.poem.userName}</h3>}
-        {props.poem.lines.map((line, i) => {
-            return <div key={i}>{line}</div>
-        })}
-        <hr></hr>
-        <div>With some help from:</div>
-        {props.poem.authors.map((author, i) => {
-            return <span key={i}>{i !== 0 && <span>, </span>}{author}</span>
-        })}
-        <div>On {props.poem.date}</div>
-    </div>
+    <Card style={{maxWidth:600, margin:'0 auto', padding: '20px 5px'}}>
+        <CardContent>
+            {props.page === 'feed' && <Typography gutterBottom variant='h5'>By {props.poem.userName}</Typography>}
+            {props.poem.lines.map((line, i) => {
+                return <div key={i}>{line}</div>
+            })}
+            <hr></hr>
+            <div>With some help from:</div>
+            {props.poem.authors.map((author, i) => {
+                return <span key={i}>{i !== 0 && <span>, </span>}{author}</span>
+            })}
+            <hr></hr>
+            <div>On {props.poem.date}</div>
+        </CardContent>
+    </Card>
   )
 }
 
