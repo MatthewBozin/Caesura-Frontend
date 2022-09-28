32e85d8118eeb92868b50783347ff63a3d391b88
Matthew Bozin <mbozin94@gmail.com>
2022-09-28 13:49:17 -0700

    Replace Landing page buttons with Material UI buttons, added Log In button to Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 const Landing = (props) => {
   return (
@@ -8,8 +9,17 @@ const Landing = (props) => {
           <span>(cae·su·ra, sēˈzyo͝orə): a break or pause in the middle of a verse</span>
           <h2>Poetry, remixed.</h2>
           {props.user ? (
-            <button onClick={() => {props.setPage("create")}}>Create</button>
-          ) : <button onClick={() => {props.setPage("signup")}}>Sign up</button>}
+            <Button type='submit' variant='contained' color='primary'
+              onClick={() => {props.setPage("create")}}>Create</Button>
+          ) : 
+          <>
+            <Button type='submit' variant='contained' color='primary'
+              onClick={() => {props.setPage("login")}}>Log In</Button>
+            <hr />
+            <Button type='submit' variant='contained' color='primary'
+              onClick={() => {props.setPage("signup")}}>Sign Up</Button>
+          </>
+          }
         </header>
     </div>
   )
